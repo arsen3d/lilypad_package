07b0e2df2a6376226cb6f3fbb01dadd393cbd729
Arsen Yeremin <arsen3d@gmail.com>
2025-02-04 18:36:41 -0800

    Update Lilypad to version 2.11.0

--- make-package.sh
+++ make-package.sh
@@ -1,5 +1,17 @@
+LATEST_VERSION=$(curl -s https://api.github.com/repos/Lilypad-Tech/lilypad/releases/latest | grep -Po '"tag_name": "v\K.*?(?=")')
+curl -L "https://github.com/Lilypad-Tech/lilypad/releases/download/v$LATEST_VERSION/lilypad-linux-amd64-cpu" -o lilypad-package/usr/local/bin/lilypad
+sed -i "s/^Version: .*/Version: $LATEST_VERSION/" lilypad-package/DEBIAN/control
+
+# Update version in Release file
+sed -i "s/^Version: .*/Version: $LATEST_VERSION/" dists/focal/Release
+
+chmod +x lilypad-package/usr/local/bin/lilypad
 dpkg-deb --build lilypad-package
 rm dists/focal/main/binary-amd64/Packages
 rm dists/focal/main/binary-amd64/Packages.gz
 dpkg-scanpackages . /dev/null > dists/focal/main/binary-amd64/Packages
 gzip -k dists/focal/main/binary-amd64/Packages
+
+git add .
+git commit -m "Update Lilypad to version $LATEST_VERSION"
+git push origin main
