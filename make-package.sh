4fb1af7cd882982f9fbe059cfeab17f61189d9bf
Arsen Yeremin <arsen3d@gmail.com>
2025-02-04 18:23:15 -0800

    update

--- make-package.sh
+++ make-package.sh
@@ -1,3 +1,5 @@
 dpkg-deb --build lilypad-package
+rm dists/focal/main/binary-amd64/Packages
+rm dists/focal/main/binary-amd64/Packages.gz
 dpkg-scanpackages . /dev/null > dists/focal/main/binary-amd64/Packages
 gzip -k dists/focal/main/binary-amd64/Packages
